c283ee36a27f28cd6b36b25d95765da5558df671
sungeu094 <127185696+sungeu094@users.noreply.github.com>
2023-09-30 11:27:10 +0900

    main_menu

--- WordPangPang/WordPangPang.js
+++ WordPangPang/WordPangPang.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   function changePage(x) {
-    const pages = ["default", "page1", "page2", "page3"];
+    const pages = ["page1", "page2", "page3", "page4"];
     for (i = 0; i < pages.length; i++) {
       if (i == x) {
         $('#' + pages[i]).show();
@@ -11,19 +11,46 @@ $(document).ready(() => {
     }
   }
 
-  $("h1").click(() => {
-    changePage(0);
-  });
 
   $("#btn1").click(() => {
-    changePage(1);
+    //$('#btn1').css('background-color', 'green');
+    changePage(0);
+    $('#main').hide(); // 버튼 클릭 시 main 요소를 숨김
   });
 
   $("#btn2").click(() => {
-    changePage(2);
+    changePage(1);
+    $('#main').hide();
   });
 
   $("#btn3").click(() => {
-    changePage(3);
+    changePage(2);
+    $('#main').hide();
   });
+
+  $("#btn4").click(() => {
+    changePage(3);
+    $('#main').hide();
+  });
+
 });
+
+function MoveTo_menu(x){
+    const menu = ["단어장 열람", "단어퀴즈", "미니 게임", "종료"];
+    const menu_btn = ["btn1", "btn2", "btn3", "btn4"];
+    //var menu_color = $('#' + menu_btn[x]).css('color', 'green');
+    if(confirm("이동하시겠습니까?") == true){
+      alert(menu[x] + "(으)로 이동하겠습니다.");
+    }else{
+      alert(menu[x] + "(으)로 이동하지 않겠습니다.");
+    }
+}
+
+function BackTo_menu(x){
+  const page = ["page1", "page2", "page3", "page4"];
+  $('#'+page[x]).hide();
+  $('#main').show();
+}
+
+
+
